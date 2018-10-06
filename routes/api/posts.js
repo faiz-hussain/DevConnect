339683e596dd3123b9798f92a944ce185faeb05b
Faizal (Zayd) Hussain <faizal.hussain33@gmail.com>
2018-10-06 19:32:32 -0400

    ✨Create add/remove comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -131,4 +131,61 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
         .catch(err => res.status(400).json({ postnotfound: 'Post not found' }));
     });
 });
+
+
+// Route: POST req to api/posts/comment/:id
+// Description: Add a comment to a post
+// Access: Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    //If any errors return, send 400 with errors object
+    return res.status(400).json(errors);
+  };
+
+  Post.findById(req.params.id)
+    .then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      }
+
+      //Add to comments array
+      post.comments.unshift(newComment);
+      post.save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ postnotfound: 'No post was found' }));
+    })
+});
+
+
+// Route: DELETE req to api/posts/comment/:id/:comment_id
+// Description: Delete a comment from a post
+// Access: Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  Post.findById(req.params.id)
+    .then(post => {
+      //Check to see if comment exists
+      if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({ commentnotexist: 'Comment does not exist' });
+      }
+
+      //Ger remove index
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+      //Splice comment out of array
+      post.comments.splice(removeIndex, 1);
+
+      post.save().then(post => res.json(post));
+    })
+});
+
 module.exports = router;
