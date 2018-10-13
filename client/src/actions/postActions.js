1926319474958bc810eaeef8cd88b4d8d61f9130
Faizal (Zayd) Hussain <faizal.hussain33@gmail.com>
2018-10-12 21:46:58 -0400

    ✨Finalize comment functions and polish

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING, DELETE_POST, GET_POST } from './types';
+import { ADD_POST, GET_ERRORS, CLEAR_ERRORS, GET_POSTS, POST_LOADING, DELETE_POST, GET_POST } from './types';
 
 //Add post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios.post('/api/posts', postData)
     .then(res =>
       dispatch({
@@ -95,9 +96,52 @@ export const removeLike = id => dispatch => {
     );
 };
 
+//Add comment
+export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
+  axios.post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Delete comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 //Set loading state
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  }
+}
+
+//Clear errors
+
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   }
 }
